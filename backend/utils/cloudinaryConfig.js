36e756e3c2e648979ea1c6496514cec403d5e835
Mahendra2611 <stpmpv30@gmail.com>
2024-09-14 17:37:34 +0530

    removed data

--- backend/utils/cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-    cloud_name: "dhu4gzm7k",
-    api_key: "698411874675693",
-    api_secret: "7TruPMU1vOfxrl_TCYb7c6X-clw",
-  });
-  
-export default cloudinary
