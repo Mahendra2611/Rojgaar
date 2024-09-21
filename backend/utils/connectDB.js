a1b0fb9b2d53a6fc09012903946a56070f8ee755
Mahendra2611 <132124880+Mahendra2611@users.noreply.github.com>
2024-09-21 12:15:15 +0530

    Update connectDB.js

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
 export default async function connectDB(){
      await mongoose.connect(process.env.DATABASE_URL)
-  .then(()=>console.log("database connected")
+  .then(()=>console.log("database connected"))
 }
